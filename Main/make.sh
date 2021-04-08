74c4d38041b9960ce9e9eb7a327b82ebbca03adc
Jonas P <jonaspleyer@gmx.de>
2021-04-08 08:07:32 +0200

    Better structure in makefile

--- Main/make.sh
+++ Main/make.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-texfot latexmk -pdf -shell-escape -interaction=nonstopmode -output-directory=BuildFiles main.tex
+COMPILER=latexmk
+
+OPTIONS=( -pdf -shell-escape -interaction=nonstopmode -output-directory=BuildFiles )
+
+TARGET=main.tex
+
+echo "#############################"
+echo "Using compiler " $COMPILER
+echo "With options " 
+for opt in ${OPTIONS[@]}
+do
+	echo $opt
+	#"${OPTIONS[@]}"
+done
+echo "On Target "$TARGET
+echo "#############################"
+
+$COMPILER ${OPTIONS[@]} $TARGET
